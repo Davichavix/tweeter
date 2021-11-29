f1fc97c4a2335cbedc610c9c8e5f1ec2b01ae400
David Chan <dchan902@gmail.com>
2021-11-28 21:27:35 -0700

    implemented renderTweets function for multiple tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 
 const createTweetElement = function(tweet) {
@@ -43,8 +56,13 @@ const createTweetElement = function(tweet) {
     return $tweet;
   }
 
-  const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      let $tweet = createTweetElement(tweet)
+      $('#tweets-container').append($tweet);
+    }
+  }
 
   $(document).ready(function () {
-    $('#tweets-container').append($tweet);
+    renderTweets(data);
   })
