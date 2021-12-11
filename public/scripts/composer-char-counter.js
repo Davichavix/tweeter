e714e9b30f81a560e7fd4394d23c01ecbe42eae8
David Chan <dchan902@gmail.com>
2021-12-11 16:53:41 -0700

    lint composer-char

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $('textarea#tweet-text').keyup(function() {
+  $('textarea#tweet-text').on('input', function() {
     let tweetLen = $(this).val().length;
     let tweetLenRemain = 140 - tweetLen;
-    const parentTag = $(this).parent()
+    const parentTag = $(this).parent();
     const counterTag = parentTag.find('.counter');
     if (tweetLenRemain < 0) {
       counterTag.css('color', 'red');
-      counterTag.text(tweetLenRemain)
+      counterTag.text(tweetLenRemain);
     } else if (tweetLenRemain >= 0) {
       counterTag.text(tweetLenRemain);
-      counterTag.css('color', '')
+      counterTag.css('color', '');
     }
-  })
-  console.log("This")
+  });
+  console.log("This");
 });
