7846227f922a543304a3d889cee6f18d8d50bfe9
David Chan <dchan902@gmail.com>
2021-12-12 21:56:07 -0700

    change new tweet height to auto

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 
 const escape = function (str) {
   let div = document.createElement("div");
@@ -91,11 +67,11 @@ const createTweetElement = function(tweet) {
       event.preventDefault();
       if ($('#tweet-text').val().length === 0) {
         $('.error-msg').empty();
-        $('.error-msg').append('❌ tweet cannot be empty ❌').slideDown();
+        $('.error-msg').text('❌ tweet cannot be empty ❌').slideDown();
       } 
       if ($('#tweet-text').val().length > 140) {
         $('.error-msg').empty();
-        $('.error-msg').append('❌ tweet must be 140 characters or under ❌').slideDown();
+        $('.error-msg').text('❌ tweet must be 140 characters or under ❌').slideDown();
       } else {
       const tweetSerial = $(this).serialize();
       $.post('/tweets', tweetSerial)
