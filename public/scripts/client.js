d71f81114f063719826f1ce70559d5b86120765b
David Chan <dchan902@gmail.com>
2021-12-13 19:42:26 -0700

    lint script files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,7 @@
  */
 
 // escape function to prevent cross-site scripting
-const escape = function (str) {
+const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
@@ -33,69 +33,69 @@ const createTweetElement = function(tweet) {
           <i class="fas fa-heart fa-xs"></i>
         </div>
       </footer>
-    </div>`)
+    </div>`);
     
-    return $tweet;
-  }
+  return $tweet;
+};
 
-  // function loops through all existing tweets and prepends to tweets-container
-  const renderTweets = function(tweets) {
-    $container = $('#tweets-container');
-    $container.empty();
-    for (const tweet of tweets) {
-      let $tweet = createTweetElement(tweet)
-      $container.prepend($tweet);
-    }
+// function loops through all existing tweets and prepends to tweets-container
+const renderTweets = function(tweets) {
+  $container = $('#tweets-container');
+  $container.empty();
+  for (const tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $container.prepend($tweet);
   }
+};
 
 // AJAX call to GET tweets as JSON data
 // Takes renderTweets as argument
-  const loadTweets = function(cb) {
-    $.ajax({
-      url: '/tweets',
-      method: 'GET',
-      dataType: 'json',
-      success: (tweets) => {
-        cb(tweets);
-      },
-      error : (error) => {
-        console.log(error);
-      }
-    })
-  }
+const loadTweets = function(cb) {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: 'json',
+    success: (tweets) => {
+      cb(tweets);
+    },
+    error : (error) => {
+      console.log(error);
+    }
+  });
+};
 
-  $(document).ready(function () {
+$(document).ready(function() {
 
-    // loads existing tweets on page refresh
-    loadTweets(renderTweets);
+  // loads existing tweets on page refresh
+  loadTweets(renderTweets);
 
-    // event handler for TWEET button
-    $("#tweet-forms").on('submit', function (event) {
-      event.preventDefault();
+  // event handler for TWEET button
+  $("#tweet-forms").on('submit', function(event) {
+    event.preventDefault();
       
-      // checks if tweet is empty or only blank characters
-      if ($('#tweet-text').val().length === 0 || $('#tweet-text').val().trim().length === 0) {
-        $('.error-msg').empty();
-        $('.error-msg').text('❌ tweet cannot be empty ❌').slideDown();
-      } 
+    // checks if tweet is empty or only blank characters
+    if ($('#tweet-text').val().length === 0 || $('#tweet-text').val().trim().length === 0) {
+      $('.error-msg').empty();
+      $('.error-msg').text('❌ tweet cannot be empty ❌').slideDown();
+    }
 
-      // checks if tweet is over 140 chars
-      else if ($('#tweet-text').val().length > 140) {
-        $('.error-msg').empty();
-        $('.error-msg').text('❌ tweet must be 140 characters or under ❌').slideDown();
-      } else {
-        const tweetSerial = $(this).serialize();
-
-        // if no error post new tweet to page
+    // checks if tweet is over 140 chars
+    else if ($('#tweet-text').val().length > 140) {
+      $('.error-msg').empty();
+      $('.error-msg').text('❌ tweet must be 140 characters or under ❌').slideDown();
+    } else {
+      const tweetSerial = $(this).serialize();
+      
+      // if no error post new tweet to page
         $.post('/tweets', tweetSerial)
-        .then(() => {
-          $('.error-msg').slideUp();
-          loadTweets(renderTweets);
-          $('.counter').text(140);
+          .then(() => {
+            $('.error-msg').slideUp();
+            loadTweets(renderTweets);
+            $('.counter').text(140);
 
-          // resets tweet textarea to empty on successful post
-          $('#tweet-text').val('');
+            // resets tweet textarea to empty on successful post
+            $('#tweet-text').val('');
         });
       }
-    })
-  })
+  });
+});
